9fd303ce41e1e4d7af4fcaeb6e62a7ecb1dd4091
a23022479 <964101978@qq.com>
2019-11-11 11:36:15 +0800

    初始化项目

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// 导入字体文件
+import './assets/fonts/iconfont.css'
+
+// 最后导入我们自己的样式文件
+import './index.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
