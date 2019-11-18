2557aa92433aca59482f7f704ce80b528bf5ca46
a23022479 <964101978@qq.com>
2019-11-18 11:22:14 +0800

    完成地图找房

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Layout from "./views/Layout";
 import Login from "./views/Login";
 import CityList from "./views/CityList";
 import Map from './views/Map'
-
+import Detail from './views/Detail'
 function NotFound(){
   return <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573454653412&di=038bbffbad099ed90f5243ef97c9bd26&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01565959e6bcfda80121bea5beef4c.jpg%401280w_1l_2o_100sh.jpg" />
 }
@@ -22,6 +22,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/citylist" component={CityList} />
           <Route path="/map" component={Map} />
+          <Route path="/detail/:id" component={Detail} />
           <Redirect exact from="/" to="/layout" />
           <Route component={NotFound} />
         </Switch>
